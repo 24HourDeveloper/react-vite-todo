3d3e08e5b6a7c5fcb148557b876b46d8c3df344f
24HourDeveloper <mikeitt009@gmail.com>
2024-07-25 15:13:45 -0400

    installed clerk, added clerk provider, added clerk components and styled header

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button, Flex, Heading } from '@chakra-ui/react'
 import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
 import useStore from './store'
@@ -7,8 +9,23 @@ function App() {
   const todos = useStore(state => state.todos)
   return (
     <>
-      <TodoInput />
-      <TodoList todos={todos}/>
+      <Flex as="header" justifyContent="space-between" bg="teal.800" w="100%" px="4" mb="4" py="2">
+        <Heading color="white">Todo</Heading>
+        <SignedOut>
+          <SignInButton>
+            <Button colorScheme="teal">Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </Flex>
+      <SignedIn>
+        <Flex flexDirection="column" justifyContent="center" mt="4" alignItems="center">
+          <TodoInput />
+          <TodoList todos={todos}/>
+        </Flex>
+      </SignedIn>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
+import { ClerkProvider } from '@clerk/clerk-react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ClerkProvider>
   </React.StrictMode>,
 )
